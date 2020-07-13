db873a6580114e673c8d8167708b4042473a9a9c
jjankowski <jjankowski555@gmail.com>
2020-07-13 17:33:35 +0200

    Parser made simpler with Scanner instead of reading bytes from input streams

--- src/main/java/com/jerzy/printer/Parser.java
+++ src/main/java/com/jerzy/printer/Parser.java
@@ -3,13 +3,10 @@ package com.jerzy.printer;
 import com.jerzy.printer.model.Flashcard;
 import com.jerzy.printer.model.FlashcardInfo;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Parser {
 
@@ -19,39 +16,30 @@ public class Parser {
     public static final String INFO_CATEGORY_LABEL = "kategoria: ";
     public static final String INFO_START = "***<";
     public static final String INFO_END = "]>***";
-    public static final Charset CHARSET = Charset.forName("UTF-16");
+    public static final String CHARSET_NAME = "UTF-16";
 
     public List<Flashcard> parseFlashcardsFromFile(String inputFilename) throws IOException {
         List<Flashcard> flashcards = new ArrayList<>();
-        InputStream inputStream = new FileInputStream(inputFilename);
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, CHARSET);
-        int data = inputStreamReader.read();
-        StringBuffer input = new StringBuffer();
-        while(data != -1){
-            input.append((char)(data));
-            data = inputStreamReader.read();
-            if(data == '\n' || data == -1) {
-                updateFlashcardsWithFlashcardFromInput(flashcards, input);
-                input = new StringBuffer();
-            }
+        File file = new File(inputFilename);
+        Scanner scanner = new Scanner(file, CHARSET_NAME);
+        while (scanner.hasNextLine()) {
+            updateFlashcardsWithFlashcardFromInput(flashcards, scanner.nextLine());
         }
-        inputStreamReader.close();
-
+        scanner.close();
         return flashcards;
     }
 
-    private void updateFlashcardsWithFlashcardFromInput(List<Flashcard> flashcards, StringBuffer input) {
-        String inputString = input.toString();
-        if(inputString.contains(INFO_START)) {
+    private void updateFlashcardsWithFlashcardFromInput(List<Flashcard> flashcards, String line) {
+        if(line.contains(INFO_START)) {
             FlashcardInfo flashcardInfo = new FlashcardInfo(
-                    inputString.substring(inputString.indexOf(INFO_LANGUAGE_LABEL) + 7, inputString.indexOf(INFO_LANGUAGE_LABEL) + 9),
-                    inputString.substring(inputString.indexOf(INFO_LEVEL_LABEL) + 8, inputString.indexOf(INFO_LEVEL_LABEL) + 9),
-                    inputString.substring(inputString.indexOf(INFO_CATEGORY_LABEL) + 11, inputString.indexOf(INFO_END)));
-            inputString = inputString.substring(inputString.indexOf(INFO_END) + 5);
+                    line.substring(line.indexOf(INFO_LANGUAGE_LABEL) + 7, line.indexOf(INFO_LANGUAGE_LABEL) + 9),
+                    line.substring(line.indexOf(INFO_LEVEL_LABEL) + 8, line.indexOf(INFO_LEVEL_LABEL) + 9),
+                    line.substring(line.indexOf(INFO_CATEGORY_LABEL) + 11, line.indexOf(INFO_END)));
+            line = line.substring(line.indexOf(INFO_END) + 5);
             flashcards.add(
                     new Flashcard(
-                            inputString.split(SIDES_SPLITTER)[0].replace("\n", ""),
-                            inputString.split(SIDES_SPLITTER)[1],
+                            line.split(SIDES_SPLITTER)[0].replace("\n", ""),
+                            line.split(SIDES_SPLITTER)[1],
                             flashcardInfo));
         }
     }
